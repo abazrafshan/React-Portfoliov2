58c54fc375de30606440d230534f91d90eace97c
Arash B <abazrafshan123@gmail.com>
2020-07-08 17:39:14 -0500

    Updates

---
+++ precache-manifest.d48734a6470505c49c49507d3814ba34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b8d07b73464e2bd65c52bb6c9cf5b03",
+    "url": "/React-Portfoliov2/index.html"
+  },
+  {
+    "revision": "0bc2720b7889bbe70349",
+    "url": "/React-Portfoliov2/static/css/main.683ea53e.chunk.css"
+  },
+  {
+    "revision": "33b213c15ad0b97f3b4b",
+    "url": "/React-Portfoliov2/static/js/2.0297259e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfoliov2/static/js/2.0297259e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bc2720b7889bbe70349",
+    "url": "/React-Portfoliov2/static/js/main.041b2f84.chunk.js"
+  },
+  {
+    "revision": "2c158a4ef6770a169bfb",
+    "url": "/React-Portfoliov2/static/js/runtime-main.36287ab9.js"
+  }
+]);
