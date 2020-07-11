87ac0387846928e473c58dc5401a87dc2c3438fc
Arash B <abazrafshan123@gmail.com>
2020-07-11 00:09:35 -0500

    Fixed errors with routes and image rendering

--- reactportfolio/src/components/AboutMe/AboutMe.js
+++ reactportfolio/src/components/AboutMe/AboutMe.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./AboutMe.css";
+import aboutme from "../../Assets/Images/aboutme.jpg";
 
 export default function AboutMe(){
     return (
@@ -11,13 +12,13 @@ export default function AboutMe(){
                         <h3 className="textcolor block-header">About Me</h3>
                         <hr />
                         {/* Profile picture with margin to the right of image to distance verbage from image  */}
-                        <img src="../../Assets/Images/aboutme.jpg" alt="profile pic" id="about-image" className="float-left margin-right"/>
-                        <p>Hi everyone! My name is Arash, I'm a Minnesota native living in the West Coast for the past six years, with the last three of those years residing in the Los Angeles area. </p>
+                        <img src={aboutme} alt="profile pic" id="about-image" className="float-left margin-right"/>
+                        <p className="textalignleft">Hi everyone! My name is Arash, I'm a Minnesota native living in the West Coast for the past six years, with the last three of those years residing in the Los Angeles area. </p>
                         
-                        <p>I'm excited for this journey and becoming a full stack developer! Feel free to browse this page and explore my projects, more to come!</p>
-                        <a href = "https://www.linkedin.com/in/arashbazrafshan/">My LinkedIn page</a><br></br>
-                        <a href = "https://github.com/abazrafshan">My Github Profile</a><br></br>
-                        <a href = "../../Assets/Images/Arash Bazrafshan - Resume 1.pdf">My Resume</a>
+                        <p className="textalignleft">I'm excited for this journey and becoming a full stack developer! Feel free to browse this page and explore my projects, more to come!</p>
+                        <a className="textalignleft" href = "https://www.linkedin.com/in/arashbazrafshan/">My LinkedIn page</a><br></br>
+                        <a className="textalignleft" href = "https://github.com/abazrafshan">My Github Profile</a><br></br>
+                        <a className="textalignleft" href = "../../Assets/Images/Arash Bazrafshan - Resume 1.pdf">My Resume</a>
                         </div>
                     </div>
                 </div>
--- reactportfolio/src/components/Navbar/Navbar.js
+++ reactportfolio/src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Navbar.css";
+import { Link } from 'react-router';
 
 export default function Navbar(){
     return (
@@ -14,13 +15,13 @@ export default function Navbar(){
                 <ul className="navbar-nav mr-auto">
                     {/* List will redirect user to each of the three pages of this web site */}
                     <li className="nav-item">
-                    <a className="nav-link" href="index.html">About Me <span className="sr-only">(current)</span></a>
+                    <a className="nav-link" href="about">About Me <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="portfolio.html">Portfolio <span className="sr-only">(current)</span></a>
+                    <a className="nav-link" href="portfolio">Portfolio <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="contact.html">Contact <span className="sr-only">(current)</span></a>
+                    <a className="nav-link" href="contact">Contact <span className="sr-only">(current)</span></a>
                     </li>
                 </ul>
             </span>
@@ -28,3 +29,4 @@ export default function Navbar(){
         </div>
     )
 }
+
