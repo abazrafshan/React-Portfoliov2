2a39bc437bde351e6c3614dece2711a865ab6e9e
Arash B <abazrafshan123@gmail.com>
2020-07-13 01:02:39 -0500

    Set responsiveness of portfolio page

--- reactportfolio/src/components/AboutMe/AboutMe.js
+++ reactportfolio/src/components/AboutMe/AboutMe.js
@@ -15,7 +15,7 @@ export default function AboutMe(){
                         <img src={aboutme} alt="profile pic" id="about-image" className="float-left margin-right"/>
                         <p className="textalignleft">Hi everyone! My name is Arash, I'm a Minnesota native living in the West Coast for the past six years, with the last three of those years residing in the Los Angeles area. </p>
                         
-                        <p className="textalignleft">I'm excited for this journey and becoming a full stack developer! Feel free to browse this page and explore my projects, more to come!</p>
+                        <p className="textalignleft">I'm a full stack developer with a passion for creating immersive user experiences! I have experience as a full-stack MERN developer, and am open to an opportunity to leverage my capabilities for you. Feel free to browse my site and explore my projects. Reach out anytime!</p>
                         <a className="textalignleft" href = "https://www.linkedin.com/in/arashbazrafshan/">My LinkedIn page</a><br></br>
                         <a className="textalignleft" href = "https://github.com/abazrafshan">My Github Profile</a><br></br>
                         <a className="textalignleft" href = "../../Assets/Images/Arash Bazrafshan - Resume 1.pdf">My Resume</a>
