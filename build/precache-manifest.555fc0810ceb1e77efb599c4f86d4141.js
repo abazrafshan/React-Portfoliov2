af362ff33cba91d380cb3dee4899d7793f104565
bryanswarthout <bryanswarthout@github.com>
2020-07-21 15:42:27 -0700

    fix deplu issue

---
+++ build/precache-manifest.555fc0810ceb1e77efb599c4f86d4141.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb2c40c40df43e043f5d86d1fed05cec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9589da9cb898db8b6011",
+    "url": "/static/css/main.00c6e1a5.chunk.css"
+  },
+  {
+    "revision": "c1e23bfc2ce36a57b0f8",
+    "url": "/static/js/2.46c2815b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.46c2815b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9589da9cb898db8b6011",
+    "url": "/static/js/main.da77fd2c.chunk.js"
+  },
+  {
+    "revision": "dd0ad302ca727c93b192",
+    "url": "/static/js/runtime-main.4e7c19c6.js"
+  },
+  {
+    "revision": "8c68d0778b734fca6f6b710b8a914c62",
+    "url": "/static/media/Burgerlogger.8c68d077.png"
+  },
+  {
+    "revision": "51279603c7672228d0f216dadcf54010",
+    "url": "/static/media/S7LT1i.51279603.jpg"
+  },
+  {
+    "revision": "139dd497d53f9849fd63f9d5fccf9cbd",
+    "url": "/static/media/YouMDb.139dd497.png"
+  },
+  {
+    "revision": "aafc75fb820701e76b0394d195d31560",
+    "url": "/static/media/aboutme.aafc75fb.jpg"
+  },
+  {
+    "revision": "10807f6757ba754346310c6e85d5c2fd",
+    "url": "/static/media/outerspacehero.10807f67.jpeg"
+  },
+  {
+    "revision": "46a849f52f4a15a3b7f2884e36f5b752",
+    "url": "/static/media/spacehubthumbnail.46a849f5.png"
+  },
+  {
+    "revision": "51a5a4f0e29b910e8ed7e2ebb746f93c",
+    "url": "/static/media/thievesmonopoly.51a5a4f0.png"
+  },
+  {
+    "revision": "9a9955e0f524137b27523cce3cf43756",
+    "url": "/static/media/weather.9a9955e0.png"
+  },
+  {
+    "revision": "c6e1137707a83cabddc5f4e126c70d81",
+    "url": "/static/media/workday.c6e11377.png"
+  }
+]);
